8fab16e78956c9bd49dd6cd287a9af1db61e2988
Marcelo <mvrozanti@hotmail.com>
2019-06-29 18:53:08 -0300

    machine is a function

--- setup_rat.py
+++ setup_rat.py
@@ -9,17 +9,17 @@ if not auto:
 
 s('pip install -r requirements.txt')
 
-if machine == '':
+if machine() == '':
     print('\nUnable to determine platform.\n')
     exit(1)
-elif machine == 'i386':
+elif machine() == 'i386':
     fileA = 'https://download.lfd.uci.edu/pythonlibs/h2ufg7oq/PyAudio-0.2.11-cp37-cp37m-win32.whl'
     fileB = 'https://download.lfd.uci.edu/pythonlibs/h2ufg7oq/pyHook-1.5.1-cp37-cp37m-win32.whl'
-elif machine == 'amd64':
+elif machine() == 'amd64':
     fileA = 'https://download.lfd.uci.edu/pythonlibs/h2ufg7oq/PyAudio-0.2.11-cp37-cp37m-win_amd64.whl'
     fileB = 'https://download.lfd.uci.edu/pythonlibs/h2ufg7oq/pyHook-1.5.1-cp37-cp37m-win_amd64.whl'
 else:
-    print('\n\nYou are probably running a processor like ARM: "' + machine + '". This isn\'t supported due to the lack of dependencies supporting ARM.')
+    print('\n\nYou are probably running a processor like ARM: "' + machine() + '". This isn\'t supported due to the lack of dependencies supporting ARM.')
 
 s('pip install '+fileA)
 s('pip install '+fileB)
